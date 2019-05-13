8a89e364639e01645e050a5795deab896a80c290
jarlaksy <jarlaksy@gmail.com>
2019-05-13 21:07:01 +0300

    11

--- A-4-Garbage-Collection/Practice.cs
+++ A-4-Garbage-Collection/Practice.cs
@@ -15,6 +15,7 @@ namespace Advenced.Lesson_4
         /// </summary>
         public static void AL4_P1_P2_P3_5_InstanceCounter()
         {
+
         }
 
         /// <summary>
@@ -23,6 +24,54 @@ namespace Advenced.Lesson_4
         /// </summary>
         public static void AL4_P4_P5_5_InstanceCounter()
         {
+            //User user;
+
+            for (int i = 0; i < 5000000; i++)
+            {
+                var user = new User();
+               
+                    if (i % 5000 == 0)
+                    {
+                        //Console.WriteLine("memory before"+ GC.GetTotalMemory(false));
+                        Console.WriteLine(User.counter);
+                        // GC.Collect(0);
+                        // Console.WriteLine("memory 0  " + GC.GetTotalMemory(false));
+                        // GC.Collect(1);
+                        // Console.WriteLine("memory 1  " + GC.GetTotalMemory(false));
+                        // GC.Collect(2);
+                        //Console.WriteLine("memory 2  " + GC.GetTotalMemory(false));
+                    }
+
+                //user.Dispose();
+                Console.WriteLine(User.counter);
+            }
+
         }
+
+        public class User:IDisposable
+        {
+            public static int counter = 0;
+
+            bool Isdisposed { get; set; }
+
+            public void Dispose()
+            {
+                Isdisposed = true;
+                counter--;
+                GC.SuppressFinalize(this);
+            }
+            public User()
+            {
+                counter++;
+            }
+            ~User()
+            {
+                //counter--;
+                if (Isdisposed == false) { Dispose(); }
+                Dispose();
+            }
+        }
+
+
     }
 }
